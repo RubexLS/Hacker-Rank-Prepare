e151fd9568bbcf3358adecca292daf9f1c6826f6
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-06-26 21:45:41 -0400

    ejercicios de hacker rank basicos

---
+++ Algorithms/Simple-Array-Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿string? outputPath = Environment.GetEnvironmentVariable("OUTPUT_PATH");
+
+if (string.IsNullOrEmpty(outputPath))
+{
+    outputPath = "output_local.txt";
+}
+
+using TextWriter textWriter = new StreamWriter(outputPath, true);
+
+bool verify = false;
+List<int> ar = new List<int>();
+
+Console.WriteLine("Ingrese el numero de elementos:");
+int arCount = int.Parse(Console.ReadLine()!.Trim());
+
+Console.WriteLine("Inserte los elementos:");
+do
+{
+    ar = Console.ReadLine()!.TrimEnd().Split(' ').Select(int.Parse).ToList();
+    if(ar.Count != arCount)
+        Console.WriteLine($"ingrese {arCount} elementos:");
+
+    else
+        verify = true;
+
+}while(verify == false);
+
+int result = simpleArraySum(ar);
+
+textWriter.WriteLine(result);
+
+Console.WriteLine($"[Local] Resultado guardado con éxito: {result}");
+
+
+
+static int simpleArraySum(List<int> ar)
+{
+    return ar.Sum();
+}
---
+++ Algorithms/Solve-Me-First/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert the first value:");
+int val1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert the second value:");
+int val2 = Convert.ToInt32(Console.ReadLine());
+int sum = solveMeFirst(val1,val2);
+Console.WriteLine(sum);
+
+static int solveMeFirst(int a, int b)
+{
+    return a + b;
+}
