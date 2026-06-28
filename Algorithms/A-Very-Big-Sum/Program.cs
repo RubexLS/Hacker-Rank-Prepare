cd304264b2faad704b5c4b702ce4dfac3c8a7334
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-06-27 22:43:17 -0400

    ejercicios 27_06 part 2

---
+++ Algorithms/A-Very-Big-Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿//numero de elementos de la matriz
+Console.WriteLine("Enter number elements:");
+int arCount = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+List<long> ar = new List<long>();
+bool verify = false;
+
+Console.WriteLine("Enter long elements:");
+
+//validacion del numero de elementos ingresado
+do
+{
+    ar = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+    if(ar.Count != arCount)
+        Console.WriteLine($"Enter {arCount} elements:");
+
+    else
+        verify = true;
+
+}while(verify == false);
+
+long result = aVeryBigSum(ar);
+
+Console.WriteLine($"The integer sum of the elements is {result}");
+
+static long aVeryBigSum(List<long> ar)
+{
+    return ar.Sum();
+}
---
+++ Algorithms/Diagonal-Difference/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter size array nxn");
+int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+List<List<int>> arr = new List<List<int>>();
+
+Console.WriteLine("enter array:");
+
+// validacion de la matriz
+for (int i = 0; i < n; i++)
+{
+    arr.Add(Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+}
+
+int result = diagonalDifference(arr, n);
+
+Console.WriteLine($"Diagonal Difference: {result}");
+
+
+static int diagonalDifference(List<List<int>> arr, int n)
+{
+    int sumPrimaryDiagonal = 0;
+    int sumSecundaryDiagonal = 0;
+    int j = 1;
+    for(int i = 0; i < n; i++)
+    {
+        sumPrimaryDiagonal += arr[i][i];
+        sumSecundaryDiagonal += arr[i][n-j];
+        j++;
+    }
+
+    return Math.Abs(sumPrimaryDiagonal - sumSecundaryDiagonal);
+}
