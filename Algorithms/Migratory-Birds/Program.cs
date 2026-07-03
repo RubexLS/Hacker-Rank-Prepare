ef204756bd188ee96386a3e66d7e73928b6d24ce
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-07-02 23:03:26 -0400

    Migratory Birds

---
+++ Algorithms/Migratory-Birds/Program.cs
@@ -0,0 +1,52 @@
+﻿List<int> arr = new List<int>();
+
+Console.WriteLine("Enter the amount of bird sightings.");
+int arrCount = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+do
+{
+    Console.WriteLine($"Enter {arrCount} records.");
+    arr = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+}while(arr.Count != arrCount);
+
+int result = migratoryBirds(arr);
+
+Console.WriteLine($"Type {result}");
+
+
+
+static int migratoryBirds(List<int> arr)
+{
+    List<int> newArr = new List<int>();
+    List<int> frequency = new List<int>();
+    newArr = arr.Distinct().ToList();
+
+    int maxRepeated = 0;
+    foreach(int i in newArr)
+    {
+        int count = 0;
+        foreach (int j in arr)
+        {
+            if(i == j)
+                count++;
+            
+        }
+        if(count > maxRepeated)
+            maxRepeated = count;
+    }
+
+    foreach(int i in newArr)
+    {
+        int count = 0;
+        foreach (int j in arr)
+        {
+            if(i == j)
+                count++;
+            
+        }
+        if(count == maxRepeated)
+            frequency.Add(i);
+    }
+
+    return frequency.Min();
+}
