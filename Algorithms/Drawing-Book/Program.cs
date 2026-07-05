66488ebfa3b9ceb2a38b02bceb1c42efbe6ecd72
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-07-04 23:37:35 -0400

    Drawing Book

---
+++ Algorithms/Drawing-Book/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter the number of pages in the book");
+int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+Console.WriteLine("Enter the page number to turn to ");
+int p = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+int result = pageCount(n, p);
+
+Console.WriteLine($"The minimum number of pages to turn: {result}");
+
+
+
+static int pageCount(int n, int p)
+{
+    int desdeInicio = p / 2;
+        
+    int desdeFinal = (n / 2) - (p / 2);
+
+    return Math.Min(desdeInicio, desdeFinal);
+}
