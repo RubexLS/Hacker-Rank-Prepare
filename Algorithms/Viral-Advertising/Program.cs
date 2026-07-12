0643867875998fe42257970022dba49e89288108
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-07-11 20:08:42 -0400

    Viral Advertising

---
+++ Algorithms/Viral-Advertising/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("The day number.");
+int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+int result = viralAdvertising(n);
+
+Console.WriteLine($"The cumulative likes at that day: {result}");
+
+
+
+static int viralAdvertising(int n)
+{
+    int peopleLiked = 5/2;
+    int cumulative = peopleLiked;
+    int i = 1;
+    if(n > 1)
+    {
+        do
+        {
+            i++;
+            int share = peopleLiked*3;
+            peopleLiked = share/2;
+            cumulative += peopleLiked;
+        }while(i != n);
+        return cumulative;
+    }
+    else
+        return cumulative;
+}
