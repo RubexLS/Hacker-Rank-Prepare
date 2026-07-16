b0621e01042450ceaaec68b6700486b180636872
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-07-15 20:16:56 -0400

    Extra Long Factorials

---
+++ Algorithms/Extra-Long-Factorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+Console.WriteLine("Enter a single integer");
+int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+extraLongFactorials(n);
+
+
+
+static void extraLongFactorials(int n)
+{
+    BigInteger factorial = (BigInteger)1;
+    for(int i = 25; i > 0; i--)
+        factorial *= (BigInteger)i;
+
+    Console.WriteLine($"The factorial of {n}: {factorial}");
+}
