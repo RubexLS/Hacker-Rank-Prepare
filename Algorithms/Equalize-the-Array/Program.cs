e8996537b2510216c07217d2992212561a8fd0f8
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-08-20 18:58:04 -0400

    Equalize the Array

---
+++ Algorithms/Equalize-the-Array/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+List<int> arr = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+int result = equalizeArray(arr);
+
+Console.WriteLine(result);
+
+
+
+static int equalizeArray(List<int> arr)
+{
+    int veryRepeat = arr.GroupBy(n => n).OrderByDescending(g => g.Count()).Select(g => g.Key).FirstOrDefault();
+    List<int> arrayAux = arr.Where(n => n != veryRepeat).ToList();
+
+    return arrayAux.Count();
+}
