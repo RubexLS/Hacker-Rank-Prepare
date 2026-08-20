911d848bb7dec3b310d54b4050e029e567eb099a
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-08-20 17:25:01 -0400

    correccion_Jumping on the Clouds

---
+++ Algorithms/Jumping-on-the-Clouds/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+List<int> c = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(cTemp => Convert.ToInt32(cTemp)).ToList();
+
+int result = jumpingOnClouds(c);
+
+Console.WriteLine(result);
+
+
+
+static int jumpingOnClouds(List<int> c)
+{
+    int jumps = 0;
+    for(int i = 0; i < c.Count - 1; i++)
+    {
+        if(i == c.Count - 2)
+        {
+            i++;
+            jumps++;
+        }
+        else if (c[i+1] == 1)
+        {
+            i++;
+            jumps++;
+        }
+        else if(c[i+1] == 0 && c[i+2] == 0)
+        {
+            i++;
+            jumps++;
+        }else if(c[i+1] == 0 && c[i+2] == 1)
+        {
+            jumps++;
+        }
+    }
+    return jumps;
+}
