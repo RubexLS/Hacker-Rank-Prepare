d1a9b02f1800690985a34913b40a92f34f24be92
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-08-29 23:30:51 -0400

    Encryption

---
+++ Algorithms/Encryption/Program.cs
@@ -0,0 +1,35 @@
+﻿string s = Console.ReadLine()!;
+
+string result = encryption(s);
+
+Console.WriteLine(result);
+
+
+
+static string encryption(string s)
+{
+    double raiz = Math.Sqrt(s.Length);
+    int size = 0;
+    
+    if (raiz % 1 == 0)
+    {
+        size = (int)raiz;
+    }else{
+        size = (int)raiz + 1;
+    }
+
+    string textEncrypted = "";
+
+    for(int i = 0; i < size; i++)
+    {
+        string text = "";
+        for (int j = i; j<s.Length; j+= size)
+        {
+            text += s[j];
+        }
+        textEncrypted += text;
+        if(i < size-1)
+            textEncrypted += " ";
+    }
+    return textEncrypted;
+}
