b775332a9c3fd26a2232ccc8452d7a49a7929011
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-09-13 21:13:35 -0400

    Halloween Sale

---
+++ Algorithms/Halloween-Sale/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] firstMultipleInput = Console.ReadLine()!.TrimEnd().Split(' ');
+
+int p = Convert.ToInt32(firstMultipleInput[0]);
+int d = Convert.ToInt32(firstMultipleInput[1]);
+int m = Convert.ToInt32(firstMultipleInput[2]);
+int s = Convert.ToInt32(firstMultipleInput[3]);
+
+int answer = howManyGames(p, d, m, s);
+
+Console.WriteLine(answer);
+
+
+
+static int howManyGames(int p, int d, int m, int s)
+{
+    int count = 0;
+    while (s >= p)
+    {
+        if (p > m){
+            s -= p;
+            p -= d;
+            count++;
+        }else{
+            if(s >= m){
+                p = m;
+                s -= p;
+                count++;
+            }else{
+                return count;
+            }
+        }
+    }
+    return count;
+}
