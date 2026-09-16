e19b46fa7ef82a25f8d3960c0e72a62541b4c8a9
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-09-16 19:51:23 -0400

    Chocolate Feast

---
+++ Algorithms/Chocolate-Feast/Program.cs
@@ -0,0 +1,31 @@
+﻿int t = Convert.ToInt32(Console.ReadLine()!.Trim());
+
+for (int tItr = 0; tItr < t; tItr++)
+{
+    string[] firstMultipleInput = Console.ReadLine()!.TrimEnd().Split(' ');
+
+    int n = Convert.ToInt32(firstMultipleInput[0]);
+    int c = Convert.ToInt32(firstMultipleInput[1]);
+    int m = Convert.ToInt32(firstMultipleInput[2]);
+
+    int result = chocolateFeast(n, c, m);
+
+    Console.WriteLine(result);
+}
+
+
+
+static int chocolateFeast(int n, int c, int m)
+{
+    int chocolatesComidos = n / c;
+    int envolturas = chocolatesComidos;
+        
+    while (envolturas >= m)
+    {
+        int nuevosChocolates = envolturas / m;
+        chocolatesComidos += nuevosChocolates;
+        envolturas = (envolturas % m) + nuevosChocolates;
+    }
+        
+    return chocolatesComidos;
+}
