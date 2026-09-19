aa5457f63d32fd64946bd9dc1b9d93ac36a49bb4
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-09-18 21:23:52 -0400

    Lisa's Workbook

---
+++ Algorithms/Lisas-Workbook/Program.cs
@@ -0,0 +1,39 @@
+﻿string[] firstMultipleInput = Console.ReadLine()!.TrimEnd().Split(' ');
+
+int n = Convert.ToInt32(firstMultipleInput[0]);
+int k = Convert.ToInt32(firstMultipleInput[1]);
+
+List<int> arr = Console.ReadLine()!.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+ int result = workbook(n, k, arr);
+
+        Console.WriteLine(result);
+
+
+
+
+static int workbook(int n, int k, List<int> arr)
+{
+    int pageNumber = 0;
+        int specialCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            pageNumber++;
+            int problemsInChapter = arr[i];
+
+            for (int j = 1; j <= problemsInChapter; j++)
+            {
+                if (j > 1 && (j - 1) % k == 0)
+                {
+                    pageNumber++;
+                }
+
+                if (j == pageNumber)
+                {
+                    specialCount++;
+                }
+            }
+        }
+
+        return specialCount;
+}
